5c5e828d9f1d495ba028faa3fa0dff71da381dc5
Emmanuel <e.o.donate@gmail.com>
2018-12-06 10:58:46 -0800

    Add Login page

---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+export default function Login() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState(null)
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const userData = {
+            username,
+            password
+        }
+        setUser(userData)
+    }
+
+    return (
+        <div 
+        style={{
+            textAlign: 'center'
+        }}
+        >
+        <h2>Login</h2>
+        <form style={{
+            display: 'grid',
+            alignItems: 'center',
+            justifyItems: 'center'
+        }}
+        >
+            <input type="text"  placeholder="Username" onChange={event => setUsername(event.target.value)}/>
+            <input type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
+            <button type="submit">Login</button>
+        </form>
+        {user && JSON.stringify(user, null, 2)}
+        </div>
+    );
+}
